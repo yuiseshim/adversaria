6a1698fd0b515d5917921b474a1ff83696c6b62a
yuiseshim <stochasticleastactionprinciple@gmail.com>
2019-11-29 16:01:10 +0900

    Create aws.sh

---
+++ aws.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install emacs
+
+sudo apt-get install -y ddskk
+sudo apt-get install -y yaskkserv skksearch dbskkd-cdb
+sudo apt-get install -y dbskkd-cdb fcitx-skk ibus-skk 
+sudo apt-get install -y mlterm-im-skk skksearch skktools skkdic-cdb skkdic-extra uim-skk
+sudo apt-get install -y yaskkserv 
+sudo apt-get install -y skksearch 
+sudo apt-get install -y dbskkd-cdb
+
+sudo apt-get install -y make gcc libreadline-dev libsqlite3-dev build-essential libssl-dev zlib1g-dev libbz2-dev wget curl llvm libncurses5-dev libncursesw5-dev libpng-dev
